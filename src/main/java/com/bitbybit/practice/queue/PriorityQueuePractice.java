e3a9e4bbf5eacacf28238fc4ff93dbd1192c466f
liulin <liulinali@aliyun.com>
2019-10-30 20:52:16 +0800

    PriorityQueuePractice

--- src/main/java/com/bitbybit/practice/queue/PriorityQueuePractice.java
+++ src/main/java/com/bitbybit/practice/queue/PriorityQueuePractice.java
@@ -1,8 +1,18 @@
 package com.bitbybit.practice.queue;
 
+/**
+ * 优先级队列练习
+ *
+ * @author liulin
+ */
 public class PriorityQueuePractice {
 
-//    public
+    public static void main(String[] args) {
 
+        int forCount = 100;
+        for (int i = 100; i < forCount; i--) {
+
+        }
+    }
 
 }
