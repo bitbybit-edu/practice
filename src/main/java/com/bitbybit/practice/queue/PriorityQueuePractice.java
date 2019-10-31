bbbdbc59870c59ef4658c130a8afce836258fcbf
liulin <liulinali@aliyun.com>
2019-10-31 16:20:08 +0800

    PriorityQueuePractice

--- src/main/java/com/bitbybit/practice/queue/PriorityQueuePractice.java
+++ src/main/java/com/bitbybit/practice/queue/PriorityQueuePractice.java
@@ -1,5 +1,9 @@
 package com.bitbybit.practice.queue;
 
+import java.util.Objects;
+import java.util.PriorityQueue;
+import java.util.logging.Logger;
+
 /**
  * 优先级队列练习
  *
@@ -7,12 +11,29 @@ package com.bitbybit.practice.queue;
  */
 public class PriorityQueuePractice {
 
+    private static final Logger logger = Logger.getLogger(PriorityQueuePractice.class.getSimpleName());
+
     public static void main(String[] args) {
-
+        PriorityQueue<Integer> priorityQueue = new PriorityQueue<>(100);
         int forCount = 100;
-        for (int i = 100; i < forCount; i--) {
-
+        for (int i = forCount; i > 0; i--) {
+            priorityQueue.add(i);
         }
+
+        logger.info(priorityQueue.toString());
+
+        PriorityQueue<Integer> priorityQueueCopy = new PriorityQueue<>(priorityQueue);
+        logger.info(priorityQueueCopy.toString());
+        forCount += 10;
+        for (int i = forCount; i > 0; i--) {
+            Integer poll = priorityQueueCopy.poll();
+            if (Objects.isNull(poll)) {
+                logger.info("队列中没有元素");
+                break;
+            }
+            logger.info(String.valueOf(poll));
+        }
+
     }
 
 }
