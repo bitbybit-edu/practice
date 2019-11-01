1f83f97fb936b0e51ce7dc8d4e4b6ba4ce54602e
liulin <liulinali@aliyun.com>
2019-11-01 10:31:09 +0800

    PriorityQueuePractice

--- src/main/java/com/bitbybit/practice/queue/PriorityQueuePractice.java
+++ src/main/java/com/bitbybit/practice/queue/PriorityQueuePractice.java
@@ -36,4 +36,25 @@ public class PriorityQueuePractice {
 
     }
 
+//    private void siftDownComparable(int k, E x) {
+//        Comparable<? super E> key = (Comparable<? super E>)x;
+//        int half = size >>> 1;
+//        while (k < half) { //假如下标k的元素存在左节点，则2k+1<=size,所以有k < size>>>1
+//            int child = (k << 1) + 1; //计算左节点下标
+//            Object c = queue[child];
+//            int right = child + 1;//计算右节点下标
+//            //如果左节点的值小于右节点则将c置为右节点的值，即c表示子节点中最小的值
+//            if (right < size && //表示存在右节点
+//                    ((Comparable<? super E>) c).compareTo((E) queue[right]) > 0)
+//                c = queue[child = right];
+//            //如果父节点小于或者等于最小的子节点，则终止循环
+//            if (key.compareTo((E) c) <= 0)
+//                break;
+//            //交换key和最小子节点的位置，继续往下比较子节点
+//            queue[k] = c;
+//            k = child;
+//        }
+//        queue[k] = key;
+//    }
+
 }
