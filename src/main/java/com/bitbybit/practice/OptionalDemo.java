d670a22448ba2347e51826cdba9d1678a860687d
liulin <liulin>
2020-04-26 10:01:53 +0800

    synchronized 测试

--- src/main/java/com/bitbybit/practice/OptionalDemo.java
+++ src/main/java/com/bitbybit/practice/OptionalDemo.java
@@ -4,7 +4,8 @@ import java.util.Optional;
 
 public class OptionalDemo {
     public static void main(String[] args) {
-//        Optional<String>.of(null);
+        int i = Runtime.getRuntime().availableProcessors();
+        System.out.println(i);
     }
 
     static String getMessage() {
---
+++ src/main/java/com/bitbybit/practice/thread/ThreadPoolExecutorTest.java
@@ -0,0 +1,91 @@
+package com.bitbybit.practice.thread;
+
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * 线程池
+ *
+ * @author liulin
+ * @date 2020/4/24
+ */
+public class ThreadPoolExecutorTest {
+
+    public static void main(String[] args) {
+        Integer corePoolSize = 1;
+        Integer maximumPoolSize = 2;
+        Long keepAliveTime = 10L;
+        TimeUnit timeUnit = TimeUnit.SECONDS;
+        ArrayBlockingQueue<Runnable> arrayBlockingQueue = new ArrayBlockingQueue<>(6);
+        ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutor(corePoolSize, maximumPoolSize, keepAliveTime,
+                timeUnit, arrayBlockingQueue, new ThreadPoolExecutor.AbortPolicy());
+        threadPoolExecutor.execute(() -> {
+            try {
+                System.out.println(Thread.currentThread().getId() + "正在执行");
+                long sleep = 20 * 1000L;
+                Thread.sleep(sleep);
+                System.out.println(Thread.currentThread().getId() + ":sleep=" + sleep + "执行完成");
+            } catch (Exception e) {
+
+            }
+        });
+
+        threadPoolExecutor.execute(() -> {
+            try {
+                System.out.println(Thread.currentThread().getId() + "正在执行");
+                long sleep = 20 * 1000L;
+                Thread.sleep(sleep);
+                System.out.println(Thread.currentThread().getId() + ":sleep=" + sleep + "执行完成");
+            } catch (Exception e) {
+
+            }
+        });
+        threadPoolExecutor.execute(() -> {
+            try {
+                System.out.println(Thread.currentThread().getId() + "正在执行");
+                long sleep = 20 * 1000L;
+                Thread.sleep(sleep);
+                System.out.println(Thread.currentThread().getId() + ":sleep=" + sleep + "执行完成");
+            } catch (Exception e) {
+
+            }
+        });
+
+        threadPoolExecutor.execute(() -> {
+            try {
+                System.out.println(Thread.currentThread().getId() + "正在执行");
+                long sleep = 20 * 1000L;
+                Thread.sleep(sleep);
+                System.out.println(Thread.currentThread().getId() + ":sleep=" + sleep + "执行完成");
+            } catch (Exception e) {
+
+            }
+        });
+
+        threadPoolExecutor.execute(() -> {
+            try {
+                System.out.println(Thread.currentThread().getId() + "正在执行");
+                long sleep = 20 * 1000L;
+                Thread.sleep(sleep);
+                System.out.println(Thread.currentThread().getId() + ":sleep=" + sleep + "执行完成");
+            } catch (Exception e) {
+
+            }
+        });
+
+        threadPoolExecutor.shutdown();
+
+//        threadPoolExecutor.execute(() -> {
+//            try {
+//                System.out.println(Thread.currentThread().getId() + "正在执行");
+//                long sleep = 20 * 1000L;
+//                Thread.sleep(sleep);
+//                System.out.println(Thread.currentThread().getId() + ":sleep=" + sleep + "执行完成");
+//            } catch (Exception e) {
+//
+//            }
+//        });
+
+    }
+}
