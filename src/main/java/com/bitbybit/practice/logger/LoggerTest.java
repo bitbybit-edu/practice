7de7e44dfaccc056d26437188af0002008141ec9
liulin <liulin>
2020-05-07 18:31:58 +0800

    log test

--- src/main/java/com/bitbybit/practice/logger/LoggerTest.java
+++ src/main/java/com/bitbybit/practice/logger/LoggerTest.java
@@ -10,5 +10,20 @@ public class LoggerTest {
     public static void main(String[] args) {
         logger.info("test logger");
         logger.error("test logger");
+        m();
+        A a = new A();
+        a.a();
+    }
+
+    public static void m() {
+        logger.error("test logger m method");
+    }
+}
+
+class A {
+    private static final Logger logger = LoggerFactory.getLogger(A.class);
+
+    public void a() {
+        logger.error("test logger A a method");
     }
 }
