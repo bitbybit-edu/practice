c9280cc398bf0fa04f7ce1aaf3b7ce9df0f8f936
liulin <liulin4375@dingtalk.com>
2020-07-06 17:24:16 +0800

    快速排序写完

--- src/main/java/com/bitbybit/practice/sort/QuickSort.java
+++ src/main/java/com/bitbybit/practice/sort/QuickSort.java
@@ -24,17 +24,31 @@ public class QuickSort {
 
         logger.info("初始化数组： array = {}", array);
 
-        quickSort(array, 0, array.length - 1);
-        logger.info("排序后数组： array = {}", array);
+        quickSortAsc(array, 0, array.length - 1);
+        logger.info("升序排序后数组： array = {}", array);
+
+        quickSortDesc(array, 0, array.length - 1);
+        logger.info("降序排序后数组： array = {}", array);
     }
 
-    public static int[] quickSort(int[] array, int start, int end) {
+    public static int[] quickSortAsc(int[] array, int start, int end) {
         if (end > start) {
-            int partition = partition(array, start, end);
+            int partition = partitionAsc(array, start, end);
             // 排序基准位置左边
-            quickSort(array, start, partition - 1);
+            quickSortAsc(array, start, partition - 1);
             // 排序基准位置右边
-            quickSort(array, partition + 1, end);
+            quickSortAsc(array, partition + 1, end);
+        }
+        return array;
+    }
+
+    public static int[] quickSortDesc(int[] array, int start, int end) {
+        if (end > start) {
+            int partition = partitionDesc(array, start, end);
+            // 排序基准位置左边
+            quickSortDesc(array, start, partition - 1);
+            // 排序基准位置右边
+            quickSortDesc(array, partition + 1, end);
         }
         return array;
     }
@@ -50,17 +64,41 @@ public class QuickSort {
      * @param right
      * @return
      */
-    private static int partition(int[] arr, int start, int right) {
+    private static int partitionAsc(int[] arr, int start, int right) {
         int a = arr[start];
         int index = start + 1;
         for (int i = index; i <= right; i++) {
-            if (a >= arr[i]) {
+            if (a > arr[i]) {
                 swap(arr, index, i);
                 index++;
             }
         }
         swap(arr, index - 1, start);
         return index - 1;
+    }
+
+    private static int partitionDesc(int[] arr, int start, int right) {
+//        int a = arr[start];
+//        int index = 0;
+//        for (int i = start + 1; i <= right; i++) {
+//            if (a < arr[i]) {
+//                swap(arr, index++, i);
+//
+//            }
+//        }
+//        swap(arr, index, start);
+//        return index;
+
+        int a = arr[start];
+        int index = start;
+        for (int i = start + 1; i <= right; i++) {
+            if (a < arr[i]) {
+                index++;
+                swap(arr, index, i);
+            }
+        }
+        swap(arr, index, start);
+        return index;
     }
 
     private static void swap(int[] arr, int index, int i) {
