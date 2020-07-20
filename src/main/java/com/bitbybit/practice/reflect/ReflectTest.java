94da90ddc39ff693b8ffbe1fd9f9305ca0fd6ec1
liulin <liulin4375@dingtalk.com>
2020-07-20 10:40:53 +0800

    反射api

--- src/main/java/com/bitbybit/practice/reflect/ReflectTest.java
+++ src/main/java/com/bitbybit/practice/reflect/ReflectTest.java
@@ -5,6 +5,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
 public class ReflectTest {
@@ -14,23 +15,40 @@ public class ReflectTest {
     public static void main(String[] args) {
         try {
             Class<?> powerTwo = Class.forName("com.bitbybit.practice.aliyun.PowerTwo");
-            Object o = powerTwo.newInstance();
-            Method[] methods = powerTwo.getMethods();
-            for (Method method : methods) {
-                String name = method.getName();
-                logger.info("method = {}", name);
-            }
-            Method[] declaredMethods = powerTwo.getDeclaredMethods();
-            for (Method method : declaredMethods) {
-                String name = method.getName();
-                if (name.equals("test2")) {
-                    method.invoke(o, "hello");
-                }
-                logger.info("declaredMethod = {}", name);
-            }
+//            methodTest(powerTwo);
+            fieldTest(powerTwo);
         } catch (Exception e) {
             logger.error("ReflectTest fail", e);
         }
 
+    }
+
+    private static void fieldTest(Class<?> powerTwo) {
+        Field[] fields = powerTwo.getFields();
+        for (int i = 0; i < fields.length; i++) {
+            logger.info("fields[{}], name = {}", i, fields[i].getName());
+        }
+        Field[] declaredFields = powerTwo.getDeclaredFields();
+        for (int i = 0; i < declaredFields.length; i++) {
+            logger.info("declaredFields[{}], name = {}", i, declaredFields[i].getName());
+        }
+
+    }
+
+    private static void methodTest(Class<?> powerTwo) throws InstantiationException, IllegalAccessException, InvocationTargetException {
+        Object o = powerTwo.newInstance();
+        Method[] methods = powerTwo.getMethods();
+        for (Method method : methods) {
+            String name = method.getName();
+            logger.info("method = {}", name);
+        }
+        Method[] declaredMethods = powerTwo.getDeclaredMethods();
+        for (Method method : declaredMethods) {
+            String name = method.getName();
+            if (name.equals("test2")) {
+                method.invoke(o, "hello");
+            }
+            logger.info("declaredMethod = {}", name);
+        }
     }
 }
