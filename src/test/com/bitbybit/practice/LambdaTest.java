1f9345973a4714af83627d28e61326ef1206b793
liulin <lin.liu@worldsaber.com>
2020-11-25 20:37:07 +0800

    java8练习

--- src/test/com/bitbybit/practice/LambdaTest.java
+++ src/test/com/bitbybit/practice/LambdaTest.java
@@ -2,12 +2,56 @@ package com.bitbybit.practice;
 
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 public class LambdaTest {
+
+    static class Simple {
+        private String simple;
+
+        public Simple() {
+
+        }
+
+        public Simple(String simple) {
+            this.simple = simple;
+        }
+
+        @Override
+        public String toString() {
+            return "Simple{" +
+                    "simple='" + simple + '\'' +
+                    '}';
+        }
+    }
+
+    static List<Simple> simples = new ArrayList<>();
+
+    static {
+        for (int i = 0; i < 10; i ++){
+            simples.add(new Simple(String.valueOf(i)));
+        }
+    }
 
     @Test
     public void print() {
-        MathOperation add = (a, b) -> a + b;
-        System.out.println(add.operation(3, 2));
+        int c = 1;
+        MathOperation add = (a, b) -> a + b + c;
+        System.out.println(add.operation(1, 1));
+    }
+
+    @Test
+    public void listStreamTest() {
+        List<Simple> collect = simples.stream().filter(simple -> !simple.simple.equals("1")).collect(Collectors.toList());
+        collect.forEach(System.out::println);
+
+        Map<String, Simple> collect1 = collect.stream().collect(Collectors.toMap(s -> s.simple, v -> v));
+        collect1.forEach((k,v) -> System.out.println(k + "," + v));
+
+
     }
 
     interface MathOperation {
