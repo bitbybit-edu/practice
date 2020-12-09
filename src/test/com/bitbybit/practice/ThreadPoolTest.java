0c326bcce8002f8aa1518e74bbac8c9571be1fff
liulin <liulinali@aliyun.com>
2020-12-09 23:25:01 +0800

    线程池

--- src/test/com/bitbybit/practice/ThreadPoolTest.java
+++ src/test/com/bitbybit/practice/ThreadPoolTest.java
@@ -2,6 +2,7 @@ package com.bitbybit.practice;
 
 import org.junit.Test;
 
+import java.util.Random;
 import java.util.concurrent.*;
 import java.util.concurrent.atomic.AtomicLong;
 
@@ -10,8 +11,8 @@ public class ThreadPoolTest {
     @Test
     public void threadPoolCreateTest() throws ExecutionException, InterruptedException {
         ThreadFactoryImpl threadFactory = new ThreadFactoryImpl("test");
-        ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutor(1 << 2, 1 << 4, 5, TimeUnit.SECONDS,
-                new ArrayBlockingQueue<>(1 << 2), threadFactory, new ThreadPoolExecutor.CallerRunsPolicy());
+        ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutor(1 << 1, 1 << 4, 5, TimeUnit.SECONDS,
+                new ArrayBlockingQueue<>(1 << 1), threadFactory, new ThreadPoolExecutor.CallerRunsPolicy());
 
 
         CallableImpl callable = new CallableImpl(1000L);
@@ -22,15 +23,23 @@ public class ThreadPoolTest {
         Future<Long> submit5 = threadPoolExecutor.submit(callable);
         Future<Long> submit6 = threadPoolExecutor.submit(callable);
         Future<Long> submit7 = threadPoolExecutor.submit(callable);
-//        threadPoolExecutor.shutdown();
-//        System.out.println(submit1.get());
-//        System.out.println(submit2.get());
-//        System.out.println(submit3.get());
-//        System.out.println(submit4.get());
-//        System.out.println(submit5.get());
-//        System.out.println(submit6.get());
-        System.out.println(submit7.get());
+        Future<Long> submit8 = threadPoolExecutor.submit(callable);
+        Future<Long> submit9 = threadPoolExecutor.submit(callable);
+        Future<Long> submit10 = threadPoolExecutor.submit(callable);
+//        Future<Long> submit11 = threadPoolExecutor.submit(callable);
+//        Future<Long> submit12 = threadPoolExecutor.submit(callable);
+//        Future<Long> submit13 = threadPoolExecutor.submit(callable);
+//        Future<Long> submit14 = threadPoolExecutor.submit(callable);
+//        Future<Long> submit15 = threadPoolExecutor.submit(callable);
 
+        System.out.println(submit1.get());
+        System.out.println(submit2.get());
+        System.out.println(submit3.get());
+        System.out.println(submit4.get());
+        System.out.println(submit5.get());
+        System.out.println(submit6.get());
+        System.out.println(submit7.get());
+        threadPoolExecutor.shutdown();
     }
 
     class CallableImpl implements Callable<Long> {
@@ -44,8 +53,9 @@ public class ThreadPoolTest {
         public Long call() throws Exception {
             while (total > 0) {
                 total--;
+                Thread.sleep(500L);
+                System.out.println("thread name : " + Thread.currentThread().getName() + "--- total : " + total);
             }
-            System.out.println("thread name : " + Thread.currentThread().getName() + "--- total : " + total);
             return total;
         }
     }
