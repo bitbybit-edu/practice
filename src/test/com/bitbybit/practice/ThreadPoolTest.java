b40d4f0abe83563499699de0ee77d08db9f02fa4
liulin <lin.liu@worldsaber.com>
2020-12-10 20:38:59 +0800

    可运行jar pom 配置

--- src/test/com/bitbybit/practice/ThreadPoolTest.java
+++ src/test/com/bitbybit/practice/ThreadPoolTest.java
@@ -10,46 +10,46 @@ public class ThreadPoolTest {
 
     @Test
     public void threadPoolCreateTest() throws ExecutionException, InterruptedException {
-        ThreadFactoryImpl threadFactory = new ThreadFactoryImpl("test");
-        ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutor(1 << 1, 1 << 4, 5, TimeUnit.SECONDS,
-                new ArrayBlockingQueue<>(1 << 1), threadFactory, new ThreadPoolExecutor.CallerRunsPolicy());
+            ThreadFactoryImpl threadFactory = new ThreadFactoryImpl("test");
+            ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutor(1 << 1, 1 << 4, 5, TimeUnit.SECONDS,
+                    new ArrayBlockingQueue<>(1 << 1), threadFactory, new ThreadPoolExecutor.CallerRunsPolicy());
 
 
-        CallableImpl callable = new CallableImpl(100L);
-        Future<Long> submit1 = threadPoolExecutor.submit(callable);
-        Future<Long> submit2 = threadPoolExecutor.submit(callable);
-        Future<Long> submit3 = threadPoolExecutor.submit(callable);
-        Future<Long> submit4 = threadPoolExecutor.submit(callable);
-        Future<Long> submit5 = threadPoolExecutor.submit(callable);
-        Future<Long> submit6 = threadPoolExecutor.submit(callable);
-        Future<Long> submit7 = threadPoolExecutor.submit(callable);
-        Future<Long> submit8 = threadPoolExecutor.submit(callable);
-        Future<Long> submit9 = threadPoolExecutor.submit(callable);
-        Future<Long> submit10 = threadPoolExecutor.submit(callable);
-//        Future<Long> submit11 = threadPoolExecutor.submit(callable);
-//        Future<Long> submit12 = threadPoolExecutor.submit(callable);
-//        Future<Long> submit13 = threadPoolExecutor.submit(callable);
-//        Future<Long> submit14 = threadPoolExecutor.submit(callable);
-//        Future<Long> submit15 = threadPoolExecutor.submit(callable);
-        Thread daemonThread = new Thread(new DaemonRunnable(threadPoolExecutor));
-        daemonThread.setDaemon(Boolean.TRUE);
-        daemonThread.start();
+            CallableImpl callable = new CallableImpl(100L);
+            Future<Long> submit1 = threadPoolExecutor.submit(callable);
+            Future<Long> submit2 = threadPoolExecutor.submit(callable);
+            Future<Long> submit3 = threadPoolExecutor.submit(callable);
+            Future<Long> submit4 = threadPoolExecutor.submit(callable);
+            Future<Long> submit5 = threadPoolExecutor.submit(callable);
+            Future<Long> submit6 = threadPoolExecutor.submit(callable);
+            Future<Long> submit7 = threadPoolExecutor.submit(callable);
+            Future<Long> submit8 = threadPoolExecutor.submit(callable);
+            Future<Long> submit9 = threadPoolExecutor.submit(callable);
+            Future<Long> submit10 = threadPoolExecutor.submit(callable);
+    //        Future<Long> submit11 = threadPoolExecutor.submit(callable);
+    //        Future<Long> submit12 = threadPoolExecutor.submit(callable);
+    //        Future<Long> submit13 = threadPoolExecutor.submit(callable);
+    //        Future<Long> submit14 = threadPoolExecutor.submit(callable);
+    //        Future<Long> submit15 = threadPoolExecutor.submit(callable);
+            Thread daemonThread = new Thread(new DaemonRunnable(threadPoolExecutor));
+            daemonThread.setDaemon(Boolean.TRUE);
+            daemonThread.start();
 
-        System.out.println(submit1.get());
-        System.out.println(submit2.get());
-        System.out.println(submit3.get());
-        System.out.println(submit4.get());
-        System.out.println(submit5.get());
-        System.out.println(submit6.get());
-        System.out.println(submit7.get());
-        System.out.println(submit8.get());
+            System.out.println(submit1.get());
+            System.out.println(submit2.get());
+            System.out.println(submit3.get());
+            System.out.println(submit4.get());
+            System.out.println(submit5.get());
+            System.out.println(submit6.get());
+            System.out.println(submit7.get());
+            System.out.println(submit8.get());
 
-        try {
-            Thread.sleep(10000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        threadPoolExecutor.shutdown();
+            try {
+                Thread.sleep(10000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            threadPoolExecutor.shutdown();
     }
 
     class DaemonRunnable implements Runnable {
