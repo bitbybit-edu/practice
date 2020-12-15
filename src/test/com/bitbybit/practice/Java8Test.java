b87c8f2b990a9965c1bb4fd622fc3ecb06e1600b
liulin <lin.liu@worldsaber.com>
2020-12-15 16:45:58 +0800

    日期推进 日期格式化

--- src/test/com/bitbybit/practice/Java8Test.java
+++ src/test/com/bitbybit/practice/Java8Test.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 
 import java.time.LocalDateTime;
 import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
 
 public class Java8Test {
 
@@ -16,4 +17,16 @@ public class Java8Test {
         LocalDateTime localDateTimeJST = LocalDateTime.now(ZoneId.of("JST", ZoneId.SHORT_IDS));
         System.out.println(localDateTimeJST);
     }
+
+    /**
+     * 前30天日期
+     */
+    @Test
+    public void test2() {
+        LocalDateTime localDateTime = LocalDateTime.now();
+        LocalDateTime localDateTime1 = localDateTime.minusDays(30);
+        String format = localDateTime1.format(DateTimeFormatter.ofPattern("yyyy-MM-dd hh:mm:ss:SSS"));
+        System.out.println(format);
+    }
+
 }
