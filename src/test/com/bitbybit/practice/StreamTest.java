f414daffffaa6c4d75cf779d1c64e61352b07f65
liulin <lin.liu@worldsaber.com>
2020-12-16 14:57:27 +0800

    stream 真香

--- src/test/com/bitbybit/practice/StreamTest.java
+++ src/test/com/bitbybit/practice/StreamTest.java
@@ -1,4 +1,20 @@
 package com.bitbybit.practice;
 
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class StreamTest {
+
+    @Test
+    public void multiply() {
+        List<Long> a = new ArrayList<>();
+        a.add(2L);
+        a.add(2L);
+        a.add(2L);
+        a.add(2L);
+        Long reduce = a.stream().reduce(1L, (c, d) -> c * d);
+        System.out.println(reduce);
+    }
 }
