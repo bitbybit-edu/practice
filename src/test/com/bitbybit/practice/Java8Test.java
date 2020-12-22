398fd16e735e105198cc865069fdfc71c7e445bf
liulin <lin.liu@worldsaber.com>
2020-12-22 16:57:59 +0800

    locaDatetime 增减不改变原值

--- src/test/com/bitbybit/practice/Java8Test.java
+++ src/test/com/bitbybit/practice/Java8Test.java
@@ -26,7 +26,7 @@ public class Java8Test {
         LocalDateTime localDateTime = LocalDateTime.now();
         LocalDateTime localDateTime1 = localDateTime.minusMonths(6L);
 
-        String format = localDateTime1.format(DateTimeFormatter.ofPattern("yyyy-MM-dd hh:mm:ss:SSS"));
+        String format = localDateTime1.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss:SSS"));
         System.out.println(format);
     }
 
@@ -38,10 +38,27 @@ public class Java8Test {
         LocalDateTime localDateTime = LocalDateTime.now();
         for (int i = 1; i <= 3; i++) {
             LocalDateTime localDateTime1 = localDateTime.minusDays(30 * i);
-            String format = localDateTime1.format(DateTimeFormatter.ofPattern("yyyy-MM-dd hh:mm:ss:SSS"));
+            String format = localDateTime1.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss:SSS"));
             System.out.println(format);
         }
 
     }
+
+    @Test
+    public void test4() {
+        LocalDateTime today = LocalDateTime.now();
+        LocalDateTime last = today.minusDays(30);
+        LocalDateTime middle = last.minusDays(30);
+        LocalDateTime head = middle.minusDays(30);
+        String format1 = today.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss:SSS"));
+        String format2 = last.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss:SSS"));
+        String format3 = middle.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss:SSS"));
+        String format4 = head.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss:SSS"));
+        System.out.println(format1);
+        System.out.println(format2);
+        System.out.println(format3);
+        System.out.println(format4);
+    }
+
 
 }
