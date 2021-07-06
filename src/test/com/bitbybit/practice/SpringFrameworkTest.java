98077259d1a7ae372594f8486f9b1094eb3f26f8
bitbybit <liulinali@aliyun.com>
2021-07-06 20:17:32 +0800

    config import resource

--- src/test/com/bitbybit/practice/SpringFrameworkTest.java
+++ src/test/com/bitbybit/practice/SpringFrameworkTest.java
@@ -3,9 +3,8 @@ package com.bitbybit.practice;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.*;
 
 public class SpringFrameworkTest {
 
@@ -18,6 +17,19 @@ public class SpringFrameworkTest {
         log.info(beanA.getName());
     }
 
+    @Test
+    public void test2() {
+        AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext(ConfigAndImportResource.class);
+        BeanA beanA = applicationContext.getBean("beanA", BeanA.class);
+        log.info(beanA.getName());
+
+        BeanA configAndImportResourceBeanA = applicationContext.getBean("configAndImportResourceBeanA", BeanA.class);
+        log.info(configAndImportResourceBeanA.getName());
+
+        BeanA resourceBeanA = applicationContext.getBean("resourceBeanA", BeanA.class);
+        log.info(resourceBeanA.getName());
+    }
+
     @Configuration
     public static class ConfigA {
 
@@ -28,11 +40,38 @@ public class SpringFrameworkTest {
 
     }
 
+    @Configuration
+    @Import({ConfigA.class})
+    @ImportResource("import-resource.xml")
+    public static class ConfigAndImportResource {
+        @Value("${log4j.rootLogger}")
+        private String rootLogger;
+
+        @Bean("configAndImportResourceBeanA")
+        public BeanA beanA() {
+            BeanA beanA = new BeanA();
+            beanA.setName(rootLogger);
+            return beanA;
+        }
+
+    }
+
+
+
+
     public static class BeanA {
+        public BeanA() {
+            this.name = "beanA";
+        }
+
         private String name;
 
         public String getName() {
-            return "beanA";
+            return this.name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
         }
     }
 
